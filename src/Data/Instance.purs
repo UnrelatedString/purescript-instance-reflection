a593ad27dc4cec094e07e3f92acd0b2101496569
UnrelatedString <unrelatedstring@gmail.com>
2025-06-04 18:26:28 -0400

    Wait no this so needs a class for super stuff aaaaaaaaaaaaaaaaa

--- src/Data/Instance.purs
+++ src/Data/Instance.purs
@@ -1,9 +1,13 @@
 module Data.Instance
   ( FunctorInst(..)
   , functorInst
+  , ApplyInst(..)
+  , ApplyInst'(..)
+  , applyInst
   ) where
 
 import Prelude
+import Type.Proxy (Proxy)
 
 -- TODO: ...any kind of class to tie this mess together...
 -- like there's GOT to be some elegant way to like.
@@ -15,5 +19,15 @@ newtype FunctorInst f = FunctorInst
   { map :: forall a b. (a -> b) -> f a -> f b
   }
 
-functorInst :: forall f. Functor f => FunctorInst f
-functorInst = FunctorInst { map }
+functorInst :: forall f. Functor f => Proxy f -> FunctorInst f
+functorInst _ = FunctorInst { map }
+
+data ApplyInst :: (Type -> Type) -> Type
+data ApplyInst f = ApplyInst { "Functor" :: FunctorInst f } (ApplyInst' f)
+
+newtype ApplyInst' f = ApplyInst'
+  { apply :: forall a b. f (a -> b) -> f a -> f b
+  }
+
+applyInst :: forall f. Apply f => Proxy f -> ApplyInst f
+applyInst p = ApplyInst { "Functor": functorInst p } $ ApplyInst' { apply }
