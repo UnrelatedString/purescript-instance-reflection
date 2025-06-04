bac749807d709b1d91ddd3cf90d07560aae58cea
UnrelatedString <unrelatedstring@gmail.com>
2025-06-04 18:36:41 -0400

    ...and then how am I going to actually union the supers... also wait I should definitely make them polymorphic instead of actually using the Union class LMAO

--- src/Data/Instance.purs
+++ src/Data/Instance.purs
@@ -1,33 +1,34 @@
 module Data.Instance
-  ( FunctorInst(..)
+  ( class ClassHierarchy
+  , FunctorInst(..)
   , functorInst
   , ApplyInst(..)
-  , ApplyInst'(..)
   , applyInst
   ) where
 
 import Prelude
 import Type.Proxy (Proxy)
 
--- TODO: ...any kind of class to tie this mess together...
--- like there's GOT to be some elegant way to like.
--- I don't even know. just encode superclass constraints in the first place LMAO
--- what was I just thinking about for the last 5 minutes
+class ClassHierarchy :: forall k. k -> Symbol -> Row k -> Constraint
+class ClassHierarchy inst reflects supers | inst -> reflects, inst -> supers
 
 newtype FunctorInst :: (Type -> Type) -> Type
 newtype FunctorInst f = FunctorInst
   { map :: forall a b. (a -> b) -> f a -> f b
   }
 
-functorInst :: forall f. Functor f => Proxy f -> FunctorInst f
-functorInst _ = FunctorInst { map }
+instance ClassHierarchy FunctorInst "Data.Functor (Functor)" ()
 
-data ApplyInst :: (Type -> Type) -> Type
-data ApplyInst f = ApplyInst { "Functor" :: FunctorInst f } (ApplyInst' f)
+functorInst :: forall f. Functor f => FunctorInst f
+functorInst = FunctorInst { map }
 
-newtype ApplyInst' f = ApplyInst'
+newtype ApplyInst f = ApplyInst'
   { apply :: forall a b. f (a -> b) -> f a -> f b
   }
 
-applyInst :: forall f. Apply f => Proxy f -> ApplyInst f
-applyInst p = ApplyInst { "Functor": functorInst p } $ ApplyInst' { apply }
+instance ClassHierarchy ApplyInst "Data.Apply (Apply)"
+  ( "Data.Functor (Functor)" :: FunctorInst
+  )
+
+applyInst :: forall f. Apply f => ApplyInst f
+applyInst = ApplyInst { apply }
