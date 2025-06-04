3d2294d3cc09176bc1501d46f58caa846e34d4a9
UnrelatedString <unrelatedstring@gmail.com>
2025-06-04 18:19:03 -0400

    ohhh so type variables have like. differing visibility? neat

--- src/Data/Instance.purs
+++ src/Data/Instance.purs
@@ -1,5 +1,6 @@
 module Data.Instance
-  ( FunctorRep
+  ( FunctorInst(..)
+  , functorInst
   ) where
 
 import Prelude
@@ -9,7 +10,10 @@ import Prelude
 -- I don't even know. just encode superclass constraints in the first place LMAO
 -- what was I just thinking about for the last 5 minutes
 
-newtype FunctorRep :: (Type -> Type) -> Type
-newtype FunctorRep f = FunctorRep
-  { map :: forall a b. (a -> b) -> a -> b
+newtype FunctorInst :: (Type -> Type) -> Type
+newtype FunctorInst f = FunctorInst
+  { map :: forall a b. (a -> b) -> f a -> f b
   }
+
+functorInst :: forall f. Functor f => FunctorInst f
+functorInst = FunctorInst { map }
