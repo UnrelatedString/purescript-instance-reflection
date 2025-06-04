295a449106f4ecb351c42c29fe1c1be1a5011cb6
UnrelatedString <unrelatedstring@gmail.com>
2025-06-04 17:57:06 -0400

    and now to use spago docs because Pursuit is fucking dead rest in peace

--- src/Data/Instance.purs
+++ src/Data/Instance.purs
@@ -1,15 +1,15 @@
 module Data.Instance
-  ( InTheMirror
+  ( FunctorRep
   ) where
 
 import Prelude
-import Data.Newtype (class Newtype)
 
 -- TODO: ...any kind of class to tie this mess together...
 -- like there's GOT to be some elegant way to like.
 -- I don't even know. just encode superclass constraints in the first place LMAO
 -- what was I just thinking about for the last 5 minutes
--- and I'm pretty sure it's fundamentally impossible to
--- make the InTheMirror instance a class thing
 
-data InTheMirror r
+newtype FunctorRep :: (Type -> Type) -> Type
+newtype FunctorRep f = FunctorRep
+  { map :: forall a b. (a -> b) -> a -> b
+  }
