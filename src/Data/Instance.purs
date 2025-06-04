3eade6611fb8eda2ca3586e7bbf91ecbccc88af3
UnrelatedString <unrelatedstring@gmail.com>
2025-06-04 18:59:43 -0400

    okay I'm getting SOMEWHERE now. what even do I want the thing that uses supers to look like ughh:

--- src/Data/Instance.purs
+++ src/Data/Instance.purs
@@ -1,35 +1,28 @@
 module Data.Instance
-  ( class ClassHierarchy
+  ( class ReflectInstance
+  , reflectInstance
   , FunctorInst(..)
-  , functorInst
   , ApplyInst(..)
-  , applyInst
   ) where
 
 import Prelude
 import Type.Proxy (Proxy)
 
-class ClassHierarchy :: forall k. k -> Symbol -> Row k -> Constraint
-class ClassHierarchy inst reflects supers | inst -> reflects
+class ReflectInstance :: forall k. (k -> Type) -> k -> Constraint
+class ReflectInstance reflection for | reflection -> for where
+  reflectInstance :: reflection for
 
 newtype FunctorInst :: (Type -> Type) -> Type
 newtype FunctorInst f = FunctorInst
   { map :: forall a b. (a -> b) -> f a -> f b
   }
 
-instance ClassHierarchy FunctorInst "Data.Functor (Functor)" r
-
-functorInst :: forall f. Functor f => FunctorInst f
-functorInst = FunctorInst { map }
+instance Functor f => ReflectInstance FunctorInst f where
+  reflectInstance = FunctorInst { map }
 
 newtype ApplyInst f = ApplyInst
   { apply :: forall a b. f (a -> b) -> f a -> f b
   }
 
-instance ClassHierarchy ApplyInst "Data.Apply (Apply)"
-  ( "Data.Functor (Functor)" :: FunctorInst
-  | r
-  )
-
-applyInst :: forall f. Apply f => ApplyInst f
-applyInst = ApplyInst { apply }
+instance Apply f => ReflectInstance ApplyInst f where
+  reflectInstance = ApplyInst { apply }
