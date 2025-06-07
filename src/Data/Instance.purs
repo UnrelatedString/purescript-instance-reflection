7739cc05b208b043454e34a18f8640db515e7ea4
UnrelatedString <unrelatedstring@gmail.com>
2025-06-07 08:57:19 -0400

    YESSS PLEASEEEEEE

--- src/Data/Instance.purs
+++ src/Data/Instance.purs
@@ -23,12 +23,11 @@ reflectFromNewtype
   => (for' -> for) -> reflection for'
 reflectFromNewtype _ = coerce (reflectInstance :: reflection for)
 
-class HasSupers :: forall k. (k -> Type) -> Row (k -> Type) -> Constraint
-class HasSupers reflection row
+class Has row t
+instance Cons sym t row row' => Has row t
 
-class RowHasType row t
-
-instance Cons sym t row row' => RowHasType row t
+class Satisfiable :: forall k. (k -> Type) -> Row (k -> Type) -> Constraint
+class Has row reflection <= Satisfiable reflection row
 
 newtype FunctorInst :: (Type -> Type) -> Type
 newtype FunctorInst f = FunctorInst
@@ -38,6 +37,8 @@ newtype FunctorInst f = FunctorInst
 instance Functor f => ReflectInstance FunctorInst f where
   reflectInstance = FunctorInst { map }
 
+instance Has r FunctorInst => Satisfiable FunctorInst r
+
 newtype ApplyInst :: (Type -> Type) -> Type
 newtype ApplyInst f = ApplyInst
   { apply :: forall a b. f (a -> b) -> f a -> f b
@@ -46,4 +47,4 @@ newtype ApplyInst f = ApplyInst
 instance Apply f => ReflectInstance ApplyInst f where
   reflectInstance = ApplyInst { apply }
 
-
+instance (Has r ApplyInst, Satisfiable FunctorInst r) => Satisfiable ApplyInst r
