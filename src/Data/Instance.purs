6743fdfd1d5b36327e50f60cd15410a37795a81a
UnrelatedString <unrelatedstring@gmail.com>
2025-06-07 12:27:29 -0400

    ...weirdest error yet...

--- src/Data/Instance.purs
+++ src/Data/Instance.purs
@@ -13,12 +13,12 @@ import Data.Eq (class Eq1, eq1)
 import Safe.Coerce (class Coercible, coerce)
 import Type.Proxy (Proxy)
 
-class HasSupers :: forall k. (k -> Type) -> k -> Type -> Constraint
-class HasSupers reflection for via
+class HasSupers :: forall k. Type -> (k -> Type) -> k -> Constraint
+class HasSupers via reflection for | via -> for
 
-class ReflectInstance :: forall k. (k -> Type) -> k -> Constraint
-class HasSupers reflection for (Proxy for) <= ReflectInstance reflection for where
-  reflectInstance :: reflection for
+class ReflectInstance :: forall k. Type -> (k -> Type) -> k -> Constraint
+class HasSupers via reflection for <= ReflectInstance via reflection for where
+  reflectInstance :: via -> reflection for
 
 -- | Reflect a newtype's instance as if it were an instance for the wrapped type.
 reflectFromNewtype
@@ -33,33 +33,33 @@ newtype EqInst a = EqInst
   { eq :: a -> a -> Boolean
   }
 
-instance Eq a => ReflectInstance EqInst a where
-  reflectInstance = { eq }
+instance HasSupers via EqInst a
+instance Eq a => ReflectInstance (Proxy a) EqInst a where
+  reflectInstance _ = { eq }
 
 newtype Eq1Inst :: (Type -> Type) -> Type
 newtype Eq1Inst f = Eq1Inst
   { eq1 :: forall a. Eq a => f a -> f a -> Boolean
   }
 
-instance Eq1 f => ReflectInstance Eq1Inst a where
-  reflectInstance = { eq1 }
+instance HasSupers via Eq1Inst f
+instance Eq1 f => ReflectInstance (Proxy f) Eq1Inst f where
+  reflectInstance _ = { eq1 }
 
 newtype FunctorInst :: (Type -> Type) -> Type
 newtype FunctorInst f = FunctorInst
   { map :: forall a b. (a -> b) -> f a -> f b
   }
 
-instance HasSupers FunctorInst f via
-
-instance Functor f => ReflectInstance FunctorInst f where
-  reflectInstance = FunctorInst { map }
+instance HasSupers via FunctorInst f
+instance Functor f => ReflectInstance (Proxy f) FunctorInst f where
+  reflectInstance _ = FunctorInst { map }
 
 newtype ApplyInst :: (Type -> Type) -> Type
 newtype ApplyInst f = ApplyInst
   { apply :: forall a b. f (a -> b) -> f a -> f b
   }
 
-instance HasSupers FunctorInst f via => HasSupers ApplyInst f via
-
-instance Apply f => ReflectInstance ApplyInst f where
-  reflectInstance = ApplyInst { apply }
+instance ReflectInstance via FunctorInst f => HasSupers via ApplyInst f
+instance Apply f => ReflectInstance (Proxy f) ApplyInst f where
+  reflectInstance _ = ApplyInst { apply }
